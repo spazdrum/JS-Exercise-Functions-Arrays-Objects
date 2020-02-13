7946c2843873618c0ca1b36ee6b8947b4e94e0a9
spazdrum <spazdrum@gmail.com>
2020-02-12 23:43:24 -0500

    completed sorting

--- index.js
+++ index.js
@@ -198,8 +198,13 @@ function getCarInfoById(inventory, id) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * sortCarInventory returns an inventory that is sorted by car_model, ascending [A-Z].
 */
-function sortCarInventory(/* code here */) {
-  /* code here */
+function sortCarInventory(inventory) {
+  
+  return inventory.sort((item1, item2) => {
+    if (item1.car_model > item2.car_model) return 1;
+    if (item1.car_model < item2.car_model) return -1;
+    return 0  
+  })
 }
 
 /**
@@ -211,8 +216,12 @@ function sortCarInventory(/* code here */) {
  *     (1) an array which is an inventory of cars like the one inside /data/inventory.js.
  * getModelYears returns an array containing all the 'car_year's in the inventory.
 */
-function getModelYears(/* code here */) {
-  /* code here */
+function getModelYears(inventory) {
+  const carYears = []
+  for (let i = 0; i < inventory.length; i++) {
+    carYears.push(inventory[i].car_year)
+  }
+  return carYears
 }
 
 /**
